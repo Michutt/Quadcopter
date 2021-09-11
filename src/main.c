f4f858a83cc0ae7096b912527dd38fa00a39d74c
Michutt <niemc..michal@gmail.com>
2021-09-11 15:49:50 +0200

    add MPU files with init

--- src/main.c
+++ src/main.c
@@ -1,4 +1,7 @@
 #include "pico/stdlib.h"
+#include "hardware/i2c.h"
+#include "../lib/MPU6050.h"
+
 
 #define NRF_CE      8u
 #define NRF_CSN     9u
@@ -6,15 +9,14 @@
 #define NRF_MOSI    11u
 #define NRF_MISO    12u
 
-int main() {
-    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
+#define MPU_SCL     3u 
+#define MPU_SDA     2u
 
-    while (true) {
-        gpio_put(LED_PIN, 1);
-        sleep_ms(250);
-        gpio_put(LED_PIN, 0);
-        sleep_ms(250);
+int main() {
+    MPU_init(i2c1_inst, 400000, MPU_SDA, MPU_SCL);
+
+    while (true) 
+    {
+//
     }
 }
