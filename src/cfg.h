d147ce3f4cf1f691728bd9f42375b5bf820d5090
Michutt <niemc..michal@gmail.com>
2021-09-12 12:04:38 +0200

    add  cfg file

---
+++ src/cfg.h
@@ -0,0 +1,19 @@
+#ifndef CFG_H
+#define CFG_H
+
+#define NRF_CE      8u
+#define NRF_CSN     9u
+#define NRF_CLK     10u
+#define NRF_MOSI    11u
+#define NRF_MISO    12u
+
+#define MPU_SCL     3u 
+#define MPU_SDA     2u
+#define MPU_SPEED   400000u
+
+#define MOTOR1      0u
+#define MOTOR2      1u
+#define MOTOR3      4u
+#define MOTOR4      5u
+
+#endif          // !CFG_H
--- src/main.c
+++ src/main.c
@@ -2,25 +2,11 @@
 #include "logic/motors.h"
 #include "hardware/i2c.h"
 #include "../lib/MPU6050.h"
-
-
-#define NRF_CE      8u
-#define NRF_CSN     9u
-#define NRF_CLK     10u
-#define NRF_MOSI    11u
-#define NRF_MISO    12u
-
-#define MPU_SCL     3u 
-#define MPU_SDA     2u
-
-#define MOTOR1      0u
-#define MOTOR2      1u
-#define MOTOR3      4u
-#define MOTOR4      5u
+#include "cfg.h"
 
 int main() {
     stdio_init_all();
-    MPU_init(i2c1_inst, 400000, MPU_SDA, MPU_SCL);
+    MPU_init(i2c1_inst, MPU_SPEED, MPU_SDA, MPU_SCL);
     MOTORS_init(MOTOR1, MOTOR2, MOTOR3, MOTOR4);
 
     while (true) 
